257f999e4c09e16e973b3ce2b75e0e3f1cd09d69
MeenS4 <rokoszoczek@gmail.com>
2024-07-12 17:24:00 +0200

    feat: add initial styling + html structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { GameView } from "@/lib/game/view/";
+
 export default function GamePage() {
-  return <main></main>;
+  return <GameView />;
 }
--- src/lib/game/view/game-view.tsx
+++ src/lib/game/view/game-view.tsx
@@ -1,5 +1,5 @@
 import styles from "./game-view.module.scss";
 
 export function GameView() {
-  return <main className={styles["game-view"]}></main>;
+  return <main className={styles["game-view"]}>Hello this is GameView</main>;
 }
